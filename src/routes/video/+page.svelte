aeab270d4729e2f773156a3e5f71bf652db0a59b
gbaranski <me@gbaranski.com>
2022-11-22 23:41:00 +0100

    feat: selecting polygons

--- src/routes/video/+page.svelte
+++ src/routes/video/+page.svelte
@@ -5,37 +5,83 @@
 	export let data: PageData;
 
 	let canvas: HTMLCanvasElement;
+    let image: HTMLImageElement | null = null;
 
 	onMount(() => {
 		console.log('mounted');
 		const ctx = canvas.getContext('2d')!;
-		const img = new Image();
-		img.addEventListener(
+        image = new Image();
+		image.addEventListener(
 			'load',
 			() => {
-                canvas.width = img.width;
-                canvas.height = img.height;
-				ctx.drawImage(img, 0, 0);
-				ctx.beginPath();
-				ctx.moveTo(30, 96);
-				ctx.lineTo(70, 66);
-				ctx.lineTo(103, 76);
-				ctx.lineTo(170, 15);
-				ctx.stroke();
+				canvas.width = image!.width;
+				canvas.height = image!.height;
+				ctx.drawImage(image!, 0, 0);
 			},
 			false
 		);
-		img.addEventListener('error', console.error);
-		img.src = `/api/capture?src=${encodeURIComponent(data.src!)}`;
+		image.addEventListener('error', console.error);
+		image.src = `/api/capture?src=${encodeURIComponent(data.src!)}`;
 	});
+
+	const clicks: { x: number; y: number }[] = [];
+
+	const drawPolygon = () => {
+		const ctx = canvas.getContext('2d')!;
+		ctx.fillStyle = 'rgba(100,100,100,0.5)';
+		ctx.strokeStyle = '#df4b26';
+		ctx.lineWidth = 1;
+
+		ctx.beginPath();
+		ctx.moveTo(clicks[0].x, clicks[0].y);
+		for (var i = 1; i < clicks.length; i++) {
+			ctx.lineTo(clicks[i].x, clicks[i].y);
+		}
+		ctx.closePath();
+		ctx.fill();
+		ctx.stroke();
+	};
+
+	const drawPoints = () => {
+		const ctx = canvas.getContext('2d')!;
+		ctx.strokeStyle = '#df4b26';
+		ctx.lineJoin = 'round';
+		ctx.lineWidth = 5;
+
+		for (var i = 0; i < clicks.length; i++) {
+			ctx.beginPath();
+			ctx.arc(clicks[i].x, clicks[i].y, 3, 0, 2 * Math.PI, false);
+			ctx.fillStyle = '#ffffff';
+			ctx.fill();
+			ctx.lineWidth = 5;
+			ctx.stroke();
+		}
+	};
+
+	const redraw = () => {
+		const ctx = canvas.getContext('2d')!;
+		canvas.width = canvas.width; // Clears the canvas
+		ctx.drawImage(image!, 0, 0);
+
+		drawPolygon();
+		drawPoints();
+	};
+
+	const onMouseUp = (e: MouseEvent) => {
+        clicks.push({
+            x: e.offsetX,
+            y: e.offsetY,
+        });
+        redraw();
+	};
 </script>
 
-<canvas bind:this={canvas} />
+<canvas bind:this={canvas} on:mouseup={onMouseUp} width="600" height="400" ></canvas>
 
 <style>
 	canvas {
-		width: 100%;
-		height: 100%;
-		background-color: #666;
+        background: #fff;
+        display: block;
+        margin:20px auto;
 	}
 </style>
