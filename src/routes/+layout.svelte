0364b6c96e465965379958b61b6c545abf7eb9c5
gbaranski <me@gbaranski.com>
2022-11-23 14:39:20 +0100

    feat: add analytics & title

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>Visel</title>
+    <script defer data-domain="visel.gbaranski.com" src="https://plausible.gbaranski.com/js/plausible.js"></script>
+</svelte:head>
+
+<slot/>
