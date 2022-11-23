6ecf742513e2c8805383aaf571738828e30c0967
gbaranski <me@gbaranski.com>
2022-11-23 14:15:36 +0100

    feat: add output in JSON

--- src/routes/video/+page.svelte
+++ src/routes/video/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { xlink_attr } from 'svelte/internal';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
 	let canvas: HTMLCanvasElement;
 	let ctx: CanvasRenderingContext2D;
-    let image: HTMLImageElement | null = null;
+	let image: HTMLImageElement | null = null;
 
 	onMount(() => {
 		console.log('mounted');
-        ctx = canvas.getContext('2d')!;
-        image = new Image();
+		ctx = canvas.getContext('2d')!;
+		image = new Image();
 		image.addEventListener(
 			'load',
 			() => {
@@ -26,35 +27,34 @@
 	});
 
 	let clicks: { x: number; y: number }[] = [];
-    let polygons: { x: number; y: number }[][] = [];
+	let polygons: { x: number; y: number }[][] = [];
 
 	const drawPolygon = () => {
 		ctx.fillStyle = 'rgba(100,100,100,0.5)';
 		ctx.strokeStyle = '#df4b26';
 		ctx.lineWidth = 1;
 
-        if(clicks.length > 1) {
-            ctx.beginPath();
-            ctx.moveTo(clicks[0].x, clicks[0].y);
-            for (var i = 1; i < clicks.length; i++) {
-                ctx.lineTo(clicks[i].x, clicks[i].y);
-            }
-            ctx.closePath();
-            ctx.fill();
-            ctx.stroke();
-        }
-        
-        polygons.forEach((points) => {
-            ctx.beginPath();
-            ctx.moveTo(points[0].x, points[0].y);
-            for (var i = 1; i < points.length; i++) {
-                ctx.lineTo(points[i].x, points[i].y);
-            }
-            ctx.closePath();
-            ctx.fill();
-            ctx.stroke();
-        });
-        
+		if (clicks.length > 1) {
+			ctx.beginPath();
+			ctx.moveTo(clicks[0].x, clicks[0].y);
+			for (var i = 1; i < clicks.length; i++) {
+				ctx.lineTo(clicks[i].x, clicks[i].y);
+			}
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+		}
+
+		polygons.forEach((points) => {
+			ctx.beginPath();
+			ctx.moveTo(points[0].x, points[0].y);
+			for (var i = 1; i < points.length; i++) {
+				ctx.lineTo(points[i].x, points[i].y);
+			}
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+		});
 	};
 
 	const drawPoints = () => {
@@ -62,7 +62,7 @@
 		ctx.lineJoin = 'round';
 		ctx.lineWidth = 5;
 
-        console.log({clicks});
+		console.log({ clicks });
 		for (var i = 0; i < clicks.length; i++) {
 			ctx.beginPath();
 			ctx.arc(clicks[i].x, clicks[i].y, 3, 0, 2 * Math.PI, false);
@@ -74,9 +74,9 @@
 	};
 
 	const redraw = () => {
-        console.log("redraw");
+		console.log('redraw');
 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		ctx.drawImage(image!, 0, 0);
 
 		drawPolygon();
@@ -84,32 +84,55 @@
 	};
 
 	const onClick = (e: MouseEvent) => {
-        clicks.push({
-            x: e.offsetX,
-            y: e.offsetY,
-        });
-        redraw();
+		clicks.push({
+			x: e.offsetX,
+			y: e.offsetY
+		});
+		redraw();
 	};
-    
-    const onKeypress = (e: KeyboardEvent) => {
-        if (e.key === 'Enter') {
-            polygons = [...polygons, clicks];
-            clicks = [];
-            redraw();
-        }
-    }
 
+	const onKeypress = (e: KeyboardEvent) => {
+		if (e.key === 'Enter') {
+			polygons = [...polygons, clicks];
+			clicks = [];
+			redraw();
+		}
+	};
 </script>
 
-<canvas bind:this={canvas} on:mouseup={onClick} on:keypress={onKeypress} tabindex="0" width="600" height="400" ></canvas>
-{#each polygons as points}
-    <p>- points: [{#each points as point, i} [{point.x}, {point.y}]{i+1 < points.length ? ", " : ""}{/each}]</p>
-{/each}
+<canvas
+	bind:this={canvas}
+	on:mouseup={onClick}
+	on:keypress={onKeypress}
+	tabindex="0"
+	width="600"
+	height="400"
+/>
+<code>
+	<h3>YAML</h3>
+	{#each polygons as points}
+		<p>
+			- points: [{#each points as point, i}
+				[{point.x}, {point.y}]{i + 1 < points.length ? ', ' : ''}{/each}]
+		</p>
+	{/each}
+
+	<h3>JSON</h3>
+	{JSON.stringify(
+		{
+			polygons: polygons.map((points) => ({
+				points: points.map((point) => [point.x, point.y])
+			}))
+		},
+		null,
+		'\t'
+	)}
+</code>
 
 <style>
 	canvas {
-        background: #fff;
-        display: block;
-        margin:20px auto;
+		background: #fff;
+		display: block;
+		margin: 20px auto;
 	}
 </style>
