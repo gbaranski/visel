e70b1b989bb9bf387a45c38ac01e20a2e3e8076d
gbaranski <me@gbaranski.com>
2022-11-23 15:19:26 +0100

    fix: logging stderr on failure

--- src/routes/api/capture/+server.ts
+++ src/routes/api/capture/+server.ts
@@ -15,7 +15,7 @@ export const GET: RequestHandler = async ({ url }) => {
 const fetchAndParseHLS = async (src: string): Promise<Buffer> => {
     const child = spawnSync("ffmpeg", ["-i", src, "-frames:v", "1", "-f", "image2pipe", "-"]);
     if (child.status !== 0) {
-        throw new Error(`ffmpeg exited with code ${child.status}`);
+        throw new Error(`ffmpeg exited with code ${child.status}. output: ${child.stderr}`);
     }
     return child.stdout;
 }
